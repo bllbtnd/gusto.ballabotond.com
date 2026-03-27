64434d588d611ec2a4426e3b75f87999a44c5a51
Botond Balla <ballabotond@ballabotond.com>
2026-03-27 16:22:02 +0100

    un-fix :)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,6 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://gusto.ballabotond.com',
   output: 'static',
-  trailingSlash: 'always',
-  build: {
-    format: 'directory',
-  },
   integrations: [
     react(),
     tailwind(),
